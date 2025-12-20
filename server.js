4f3889aa8c420469948bedc8a53a97de27d6709c
kashif <hussainkashif1444@gmail.com>
2025-12-21 03:02:00 +0500

    Fix static HTML serving

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-const express = require("express");
 const path = require("path");
+const express = require("express");
 const app = express();
 
-// Serve static files from the "public" folder
-const frontendPath = path.join(__dirname, "../frontend/public");
+// Correct static folder path
+const frontendPath = path.join(__dirname, "public"); // if public is in backend folder
 app.use(express.static(frontendPath));
 
-// Make sure "/" serves your index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
