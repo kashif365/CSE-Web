c5a69b7e7bf886f2270e2b9be482cb3a4ddf1bd6
kashif <hussainkashif1444@gmail.com>
2025-12-22 03:06:31 +0500

    Fix Railway 502 with favicon + handlers

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const expressLayouts = require("express-ejs-layouts");
-const { title } = require("process");
 
 const app = express();
 
@@ -15,21 +14,51 @@ app.set("layout", "layouts/main");
 // static assets
 app.use(express.static(path.join(__dirname, "public")));
 
+// favicon fix (prevents 502)
+app.get("/favicon.ico", (req, res) => res.sendStatus(204));
 
 // routes
-app.get("/", (req, res) => {res.render("pages/home", { title: "SCE-DUET" });});
-app.get('/about', (req, res) => {res.render('pages/about', { title: 'About Us' });});
-app.get('/event', (req, res) => {res.render('pages/event', { title: 'Event' });});
-app.get('/speaker', (req, res) => {res.render('pages/speaker', { title: 'Speaker' });});
-app.get('/blog', (req, res)=>{res.render('pages/blog', {title: 'Blog'});});
-app.get('/contact', (req, res) => {res.render('pages/contact', { title: 'Contact' });});
-app.get('/gallary', (req, res) => {res.render('pages/gallary', { title: 'Gallary' });});
+app.get("/", (req, res) => {
+  res.render("pages/home", { title: "SCE-DUET" });
+});
 
+app.get("/about", (req, res) => {
+  res.render("pages/about", { title: "About Us" });
+});
 
-//server
+app.get("/event", (req, res) => {
+  res.render("pages/event", { title: "Event" });
+});
+
+app.get("/speaker", (req, res) => {
+  res.render("pages/speaker", { title: "Speaker" });
+});
+
+app.get("/blog", (req, res) => {
+  res.render("pages/blog", { title: "Blog" });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("pages/contact", { title: "Contact" });
+});
+
+app.get("/gallary", (req, res) => {
+  res.render("pages/gallary", { title: "Gallary" });
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
+// error handler (fixes Railway 502)
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Internal Server Error");
+});
+
+// server
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
